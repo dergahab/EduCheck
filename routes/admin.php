cb6b6b8911b0bb0d8d3976b15e9746b4aab2de0e
dergah.ab <dergah.ab@gmail.com>
2024-10-15 23:02:29 +0400

    layouts parth

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/dashboard',[DashboardController::class, 'index']);
+Route::get('/dashboard',[DashboardController::class, 'index'])->name('dashboard');
 Route::get('test', function(){ 
  return 'test test';   
 });
