047ddbe36f8d7c806fb51f1ba817a2b28271887d
dergah.ab <dergah.ab@gmail.com>
2024-10-15 23:39:58 +0400

    service maker command

---
+++ app/Services/TestService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+Class TestService extends BaseService
+{
+    public function index()
+    {
+        //
+    }
+
+     public function store()
+    {
+        //
+    }
+
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
